b146168a50897d6f52d0c16391b4dfce45aa0347
leonardo.nascimento <leomn138@gmail.com>
2018-07-10 02:11:32 -0300

    basic security for config service

--- config/src/main/java/com/bookrecommendationsystem/config/ConfigApplication.java
+++ config/src/main/java/com/bookrecommendationsystem/config/ConfigApplication.java
@@ -1,14 +1,25 @@
 package com.bookrecommendationsystem.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
+@EnableWebSecurity
 @EnableConfigServer
 public class ConfigApplication  extends WebSecurityConfigurerAdapter {
+
+	@Value("${security.user.password}")
+	private String securityPassword;
+	@Value("${security.user.name}")
+	private String securityUsername;
+	@Value("${security.user.role}")
+	private String securityRole;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
@@ -17,6 +28,16 @@ public class ConfigApplication  extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/**/*").permitAll();
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic();
+	}
+	@Override
+	public void configure(AuthenticationManagerBuilder builder) throws Exception {
+
+		builder.inMemoryAuthentication()
+				.withUser(securityUsername)
+				.password("{noop}" + securityPassword)
+				.roles(securityRole);
 	}
 }
